374a80e7629382aba024e76014f88330f1c4b984
Aquiles Popovich <popovichaquiles@gmail.com>
2023-12-13 12:01:27 -0300

    modularizando

---
+++ src/controllers/filtersAndGet.js
@@ -0,0 +1,56 @@
+const dataBase = require('../dataBase/dataBase')
+const {User, Profile, Sport, Club} = dataBase.models
+
+const getSportsInDb = async() =>{
+    try {
+        const sports = await Sport.findAll()
+
+        if(sports) return sports
+        
+    } catch (error) {
+        throw error.message
+    }
+}
+
+const getClubsInDb = async()=>{
+    try {
+        const clubs = await Club.findAll()
+        if(clubs) return clubs
+    } catch (error) {
+        throw error.message
+    }
+}
+
+
+const filterSportInDb = async(search) => {
+    try {
+      const sports = await getSportsInDb();
+
+      return sports.filter((sport) => {
+        const regex = new RegExp(search, "i");  
+        return sport.name.match(regex);
+      });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  const filterByClubs = async(search)=>{
+    try {
+        const clubs = await getSportsInDb();
+
+      return clubs.filter((club) => {
+        const regex = new RegExp(search, "i");  
+        return club.schedule.match(regex);
+      });
+    } catch (error) {
+        throw error.message
+    }
+  }
+
+  module.exports = {
+    getSportsInDb,
+    getClubsInDb,
+    filterSportInDb,
+    filterByClubs,
+  }
--- src/controllers/getInDB.js
+++ src/controllers/getInDB.js
@@ -42,25 +42,6 @@ const getUserInDb = async(id)=>{
     }
 }
 
-const getSportsInDb = async() =>{
-    try {
-        const sports = await Sport.findAll()
-
-        if(sports) return sports
-        
-    } catch (error) {
-        throw error.message
-    }
-}
-
-const getClubsInDb = async()=>{
-    try {
-        const clubs = await Club.findAll()
-        if(clubs) return clubs
-    } catch (error) {
-        throw error.message
-    }
-}
 
 const searchByName = async(name) => {
     try {
@@ -74,40 +55,12 @@ const searchByName = async(name) => {
 
 //FILTROS
 
-const filterSportInDb = async(search) => {
-    try {
-      const sports = await getSportsInDb();
 
-      return sports.filter((sport) => {
-        const regex = new RegExp(search, "i");  
-        return sport.name.match(regex);
-      });
-    } catch (error) {
-      throw error;
-    }
-  }
-
-  const filterByClubs = async(search)=>{
-    try {
-        const clubs = await getSportsInDb();
-
-      return clubs.filter((club) => {
-        const regex = new RegExp(search, "i");  
-        return club.schedule.match(regex);
-      });
-    } catch (error) {
-        throw error.message
-    }
-  }
 
 module.exports = {
     getAllProfInDb,
     getAllUsersInDb,
     getUserInDb,
     getProfileInDb,
-    getSportsInDb,
-    getClubsInDb,
-    filterSportInDb,
-    filterByClubs,
     searchByName
 }
