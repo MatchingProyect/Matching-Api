01f54c38f795d90bc786e2fa1204a134f411ac29
Aquiles Popovich <popovichaquiles@gmail.com>
2023-12-13 11:48:07 -0300

    agregue funcion de crear club

--- src/handlers/Club/createClub.js
+++ src/handlers/Club/createClub.js
@@ -1,2 +1,14 @@
+const { addClubInDb } = require("../../controllers/addInDB");
 
+const createClub = async(req, res)=>{
+    try {
+        const {schedule, price} = req.body;
+        const newClub = await addClubInDb(schedule, price)
+        if(newClub) return res.status(200).json({status: true, newClub})
+    } catch (error) {
+        return res.status(500).json({status: true, message: error.message})
+    }
+}
+
+module.exports = createClub
 
