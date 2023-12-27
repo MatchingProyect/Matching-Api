ab8e8c28ddd9e88a6408e9d70c3e642e860c1698
JMRodriguezV98 <mriciosama@gmail.com>
2023-12-27 17:20:59 -0500

    fix users model

--- src/models/User.js
+++ src/models/User.js
@@ -41,7 +41,7 @@ module.exports = (dataBase) => {
         },
         avatarImg:{
             type: DataTypes.STRING,
-            allowNull: false
+            defaultValue: 'https://thinksport.com.au/wp-content/uploads/2020/01/avatar-.jpg'
         },
         password:{
             type: DataTypes.STRING,
