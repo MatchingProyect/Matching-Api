9e33b1d55aee624e3bc44a5f3b5db84e003f9756
JMRodriguezV98 <mriciosama@gmail.com>
2023-12-27 18:04:56 -0500

    fix description null

--- src/models/User.js
+++ src/models/User.js
@@ -49,7 +49,7 @@ module.exports = (dataBase) => {
         },
         description:{
             type: DataTypes.STRING,
-            allowNull: true
+            allowNull: false
         }
     }, {timestamps: false})
 }
