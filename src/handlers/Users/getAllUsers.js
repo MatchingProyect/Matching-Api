c7fdfbfe45068722c08ef2c0693331e4e16cfe09
SAldanaNizama <140188751+SAldanaNizama@users.noreply.github.com>
2023-12-29 09:43:12 -0500

    paginado 55

--- src/handlers/Users/getAllUsers.js
+++ src/handlers/Users/getAllUsers.js
@@ -4,7 +4,7 @@ const getAllUsers = async(req, res) =>{
     try {
         const { name, page } = req.query;
         const pageNumber = Number(page) || 1;
-        const limit = 5
+        const limit = 55
      const offset = (pageNumber - 1) * limit;
 
         if(name){
