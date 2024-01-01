23a22fed5d601aa00bbc1194f317d66fc5ae9711
Lucas Mella <lucks191@gmail.com"  git config --global user.name "LMellaVillanueva>
2024-01-01 20:28:36 -0300

    database connection

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -13,8 +13,9 @@ require('dotenv').config();
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
-  user: process.env.DB_USER,
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
+  
 });
 
 const saveGoogleUserToPostgres = async (userData) => {
