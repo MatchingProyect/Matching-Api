4e2103577ae7db954798effe9296e91ab886b61a
Aquiles Popovich <popovichaquiles@gmail.com>
2024-01-01 18:30:32 -0300

    cambios

--- src/controllers/addInDB.js
+++ src/controllers/addInDB.js
@@ -199,6 +199,7 @@ const addPaymentStatusInDb = async (name) => {
 
 const addFriendRequestInDb = async (status, UserId, FriendRId) => {
     try {
+        if(status === "false") await FriendRequest.destroy()
         const addFriendRequest = await FriendRequest.create({ status, UserId, FriendRId });
         if (addFriendRequest) return addFriendRequest;
     } catch (error) {
