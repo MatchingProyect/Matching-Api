52717b92944a2dfcc713c683e2f735c5dbc34ca5
Aquiles Popovich <popovichaquiles@gmail.com>
2024-01-02 02:33:11 -0300

    cambios en el sv

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const https = require('https');
+const path = require('path')
+const fs = require('fs')
 const server = express();
 const dataBase = require('./src/dataBase/dataBase');
 const cors = require("cors");
 const router = require('./src/routes/index');
 const bodyParser = require('body-parser');
+
 
 server.use(express.json());
 server.use(cors());
@@ -11,7 +15,15 @@ server.use(bodyParser.urlencoded({extended: true}));;
 server.use(bodyParser.json());
 server.use(router);
 
-server.listen(3000, () => {
-    dataBase.sync({force: false});
-    console.log('Listen on port 3000');
-});
+// server.listen(3000, () => {
+//     dataBase.sync({force: false});
+//     console.log('Listen on port 3000');
+// });
+const sslServer = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'cert','key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert','cert.pem'))
+},server)
+sslServer.listen(3000, () => {
+        dataBase.sync({force: false});
+        console.log('Listen on port 3000');
+    })
