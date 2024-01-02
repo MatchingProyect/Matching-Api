a4aba8e7347a1097615340733bf913f5426faa2e
Lucas Mella <lucks191@gmail.com"  git config --global user.name "LMellaVillanueva>
2024-01-02 13:58:25 -0300

    busqueda por nombre minusculas

--- src/handlers/Users/getAllUsers.js
+++ src/handlers/Users/getAllUsers.js
@@ -1,16 +1,15 @@
 const { getAllUsersInDb, searchByName } = require("../../controllers/getInDB")
 
-const getAllUsers = async(req, res) =>{
+const getAllUsers = async (req, res) => {
     try {
         const { displayName, page } = req.query;
         const pageNumber = Number(page) || 1;
         const limit = 2
         const offset = (pageNumber - 1) * limit;
-        const allName = displayName.trim();
 
-        if(allName){
-            const search = await searchByName(allName);
-            if(search) return res.status(200).json({
+        if (displayName) {
+            const search = await searchByName(displayName);
+            if (search) return res.status(200).json({
                 status: true,
                 search
             })
@@ -20,15 +19,15 @@ const getAllUsers = async(req, res) =>{
             })
 
         }
-        const allUsers = await getAllUsersInDb(offset, limit, allName)
-        if(allUsers){
-            return res.status(200).json({status: true, allUsers})
-        }else{
-            return res.status(404).json({status: false, message: 'not found'})
+        const allUsers = await getAllUsersInDb(offset, limit)
+        if (allUsers) {
+            return res.status(200).json({ status: true, allUsers })
+        } else {
+            return res.status(404).json({ status: false, message: 'not found' })
         }
     } catch (error) {
-        return res.status(500).json({status: false, message: error.message})
+        return res.status(500).json({ status: false, message: error.message })
     }
 }
 
-module.exports= getAllUsers
+module.exports = getAllUsers;
