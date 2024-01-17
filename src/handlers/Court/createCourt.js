d57ccf1e94ea9571ae6dbde4bf8e06a3b719d2af
Lucas Mella <lucks191@gmail.com"  git config --global user.name "LMellaVillanueva>
2024-01-16 22:55:16 -0300

    cancha lista

--- src/handlers/Court/createCourt.js
+++ src/handlers/Court/createCourt.js
@@ -2,11 +2,11 @@ const { addCourtInDb } = require("../../controllers/addInDB");
 
 const createCourt = async(req, res) => {
     try {
-        const {name, description, priceFee, warrantyReservation, grassType, lighting, doorsType, wallsType, reputation, horarioInicio, horarioCierre, SportId, imgClub, LocationId, ClubId} = req.body;
+        const {name, description, priceFee, warrantyReservation, grassType, lighting, doorsType, wallsType, reputation, imgClub,  horarioInicio, horarioCierre, SportId,  LocationId, ClubId} = req.body;
         
         console.log(req.body)
 
-        const courtCreated = await addCourtInDb(name, description, priceFee, warrantyReservation, grassType, lighting, doorsType, wallsType, imgClub, reputation, horarioInicio, horarioCierre, SportId, LocationId, ClubId);
+        const courtCreated = await addCourtInDb(name, description, priceFee, warrantyReservation, grassType, lighting, doorsType, wallsType, reputation, imgClub,  horarioInicio, horarioCierre, SportId, LocationId, ClubId);
 
         if(courtCreated) return res.status(200).json({
             status: true,
